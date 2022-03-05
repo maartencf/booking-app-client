855919dfa124f662cabb4353603d2d4e413b01fa
Martinus Dobbelaar <mado@netcompany.com>
2022-03-05 16:52:47 +0100

    Small UI changes

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const Header: React.FC = () => {
   return (
     <header className="header">
-      <h1>Book meeting rooms at the TV 2 HQs</h1>
+      <h1>Book meeting rooms</h1>
     </header>
   )
 }
